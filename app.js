11eb44c3c0482dd085b0f124842905dd0488aad3
aybaneze <andrea270696@gmail.com>
2019-03-25 00:34:56 -0500

    subiendo imagenes y botones

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
+let registrarme= document.getElementById('registrarme');
+let slicks = document.getElementById('slicks');
+let btns = document.getElementById('btns-registro');
 $('.slick-slider').slick({
     dots: true,
     fade: true,
     arrows: false,
     mobileFirst: true,
 });
+
+registrarme.addEventListener('click',()=>{
+    slicks.style.display='none';
+    btns.style.display='block';
+})
