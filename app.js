f5e6f880c5ceeebd07ffe33e7388e347126eabc0
aybaneze <andrea270696@gmail.com>
2019-03-30 18:15:35 -0500

    subiendo

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Initialize Firebase
+
 let registrarme= document.getElementById('registrarme');
 let slicks = document.getElementById('slicks');
 let btns = document.getElementById('btns-registro');
@@ -8,66 +10,70 @@ $('.slick-slider').slick({
     mobileFirst: true,
 });
 
+facebook.addEventListener('click',inFacebook)
+google1.addEventListener('click', inGoogle)
+
 registrarme.addEventListener('click',()=>{
+    console.log('jola')
     slicks.style.display='none';
     btns.style.display='block';
 })
 
-var platform = new H.service.Platform({
-    'app_id': 'BlZHca8LI24lxVfHfKSV',
-    'app_code': 'mdzpXDgzwv3MAkgr5yQwMg',
-    useHTTPS: true
-  });
+// var platform = new H.service.Platform({
+//     'app_id': 'BlZHca8LI24lxVfHfKSV',
+//     'app_code': 'mdzpXDgzwv3MAkgr5yQwMg',
+//     useHTTPS: true
+//   });
 
-  var pixelRatio = window.devicePixelRatio || 1;
+//   var pixelRatio = window.devicePixelRatio || 1;
 
-  var latitud, longitud;
-    // Obtain the default map types from the platform object
-    var maptypes = platform.createDefaultLayers();
-var mapContainer =document.getElementById('mapContainer');
-var map ;
-if(navigator.geolocation){
-    var success = function(position){
-     latitud = position.coords.latitude,
-        longitud = position.coords.longitude;
+//   var latitud, longitud;
+//     // Obtain the default map types from the platform object
+//     var maptypes = platform.createDefaultLayers();
+// var mapContainer =document.getElementById('mapContainer');
+// var map ;
+// if(navigator.geolocation){
+//     var success = function(position){
+//      latitud = position.coords.latitude,
+//         longitud = position.coords.longitude;
 
-        var berlinMarker = new H.map.Marker({
-            lat:latitud,
-            lng:longitud
-          });
+//         var berlinMarker = new H.map.Marker({
+//             lat:latitud,
+//             lng:longitud
+//           });
 
-       map = new H.Map(
-            mapContainer,
-            maptypes.normal.map,
-            {
-              zoom: 18,
-              center: { lng: longitud, lat: latitud  },
+//        map = new H.Map(
+//             mapContainer,
+//             maptypes.normal.map,
+//             {
+//               zoom: 18,
+//               center: { lng: longitud, lat: latitud  },
         
-            });
-            map.addObject(berlinMarker);
+//             });
+//             map.addObject(berlinMarker);
           
    
-            // Create the default UI components
+//             // Create the default UI components
         
          
-    }
-    navigator.geolocation.getCurrentPosition(success, function(error){
-        switch(error.code) {
-            case error.PERMISSION_DENIED:
-                alert("Permission denied by user.");
-                break;
-            case error.POSITION_UNAVAILABLE:
-                alert("Location position unavailable.");
-                break;
-            case error.TIMEOUT:
-                alert("Request timeout.");
-                break;
-            case error.UNKNOWN_ERROR:
-                alert("Unknown error.");
-                break;
-        }
-    });
-    }
+//     }
+//     navigator.geolocation.getCurrentPosition(success, function(error){
+//         switch(error.code) {
+//             case error.PERMISSION_DENIED:
+//                 alert("Permission denied by user.");
+//                 break;
+//             case error.POSITION_UNAVAILABLE:
+//                 alert("Location position unavailable.");
+//                 break;
+//             case error.TIMEOUT:
+//                 alert("Request timeout.");
+//                 break;
+//             case error.UNKNOWN_ERROR:
+//                 alert("Unknown error.");
+//                 break;
+//         }
+//     });
+//     }
 
 
     // Instantiate (and display) a map object:
--- firebase.js
+++ firebase.js
@@ -1,13 +1,40 @@
-const facebook = document.getElementById('facebook');
-const google = document.getElementById('google1');
+window.onload = () => {
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            window.location.href='./welcome.html'
+            // data.classList.remove("hiden");
+            // Init.classList.add("hiden");
+            // Profile.innerHTML = "<img style='height:140px;width:140px;border-radius:100px;float:center;' src='" + user.photoURL + "'/>";
+            // UserCount.innerHTML = "<p>" + user.displayName + "</p>";
+            console.log('Inicio sesion srta')
+        } else {
+            // Init.classList.remove("hiden");
+            // data.classList.add('hiden');
+            // console.log('Inicio sesion srta')
+        }
+       // valposteos()
+    });
+}
+function guardaDatos(user) {
+    var usuario = {
+        uid: user.user.uid,
+        nombre: user.user.displayName,
+        email: user.user.email,
+        foto: user.user.photoURL
+    }
+    console.log(usuario,'hey',user.user.displayName)
+    let db = firebase.database().ref('femitech/'+ user.user.uid)
+        db.set(usuario)
 
+        console.log(firebase.database().ref('femitech/'+ user),firebase.database().ref())
+}
 
 
 const inGoogle = () => {
     var provider = new firebase.auth.GoogleAuthProvider();
+    
     firebase.auth().signInWithPopup(provider).then(function (result) {
-        guardaDatos(result.user);
-        console.log('inicie sesion con google');
+        guardaDatos(result);
     }).catch(function (error) {
         var errorCode = error.code;
         var errorMessage = error.message;
@@ -24,11 +51,15 @@ const inFacebook = () => {
         'display': 'popup'
     });
 
-   window.location.href='./welcome.html';
-    firebase.auth().signInWithPopup(provider).then(function (result) {
+    firebase.auth().signInWithPopup(provider).then(function (result) {    
+        guardaDatos(result);// This gives you a Facebook Access Token. You can use it to access the Facebook API.
+     var token = result.credential.accessToken;
+  // The signed-in user info.
+     var user = result.user;
+        console.log(result,'estoy en facebook')
         
-      console.log('hola')
-        //  guardaDatos(result.user);
+      console.log('facebook')
+      //  guardaDatos(result.user);
        
     }).catch(function (error) {
         // Handle Errors here.
@@ -42,5 +73,78 @@ const inFacebook = () => {
     });
 }
 
-facebook.addEventListener('click',inFacebook)
-google1.addEventListener('click', inGoogle)
+let accept = document.getElementById('accept');
+let cards = document.getElementById('cards');
+let info = document.getElementById('info');
+let next = document.getElementById('next');
+
+let confianza = document.getElementById('confianza')
+let verificacion = document.getElementById('verificacion');
+let aceptModal = document.getElementById('aceptModal');
+let contacto = document.getElementById('contacto');
+let contactbtn = document.getElementById('contactbtn');
+
+
+accept.addEventListener('click',()=>{
+
+    cards.style.display='none';
+    info.style.display='block';
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('#numberModal.modal');
+    var instances = M.Modal.init(elems, options);
+});
+
+next.addEventListener('click',()=>{
+    let number = document.getElementById('number').value;
+    console.log(number)
+    if(number!==''){
+        console.log('number')
+        info.style.display='none';
+     confianza.style.display='block';
+    }else{
+        alert("ES IMPORTANTE PONER TU NUMERO DE TELEFONO")
+    }
+  
+})
+
+
+
+aceptModal.addEventListener('click',()=>{
+confianza.style.display='none';
+  verificacion.style.display='block';
+  
+})
+
+contactbtn.addEventListener('click',()=>{
+    let verif = document.getElementById('verif').value;
+    if(verif!==''){
+        verificacion.style.display='none';
+        contacto.style.display='block'
+    }else{
+        alert('usted ingreso un codigo de verificación incorrecto')
+    }
+    
+})
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function () {
+    $('.modal').modal();
+});
+
+
+// if($('section').hasClass('info')){
+//     $('body').css({'background':'#DAF9FB'});
+// }else{
+//     $('body').css({'background':'#76C5CE'});
+// }
+
